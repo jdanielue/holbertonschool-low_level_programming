7b51597a7adf8b1dcbd2186291ca2995f9c7e8d1
jdanielue <jdurregoe@gmail.com>
2020-09-21 04:39:17 +0000

    s2a2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,12 @@
 
 int main(void)
 {
-	char ch;
-	for (ch = "a" ;  ch <= "z" ; ch++)
+	char i;
+
+	for (i = 'a' ;  i <= 'z' ; i++)
 	{
-	putchar(ch);
+		putchar(i);
 	}
+	putchar('\n');
 return (0);
 }
