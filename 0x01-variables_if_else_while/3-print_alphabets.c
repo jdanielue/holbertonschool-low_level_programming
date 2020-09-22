31780cb5e89d4456de166e3efa2c81f4b15e1335
jdanielue <jdurregoe@gmail.com>
2020-09-22 03:06:39 +0000

    s3a2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ;  i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A' ;  i <= 'Z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+return (0);
+}
