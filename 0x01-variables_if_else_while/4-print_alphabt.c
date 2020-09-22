30c1536922e9a4da7bd4a13c67ae440482798894
jdanielue <jdurregoe@gmail.com>
2020-09-22 03:44:30 +0000

    s4a2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ;  i <= 'z' ; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+return (0);
+}
