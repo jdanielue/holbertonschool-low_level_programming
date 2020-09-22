03adcff41508586abc54c4ee960099e61a15b484
jdanielue <jdurregoe@gmail.com>
2020-09-22 04:04:32 +0000

    a6s1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+return (0);
+}
