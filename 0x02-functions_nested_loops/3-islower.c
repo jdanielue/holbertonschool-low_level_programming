8566020c6989ec5e14d75e3bcfdc621c13c4b82d
jdanielue <jdurregoe@gmail.com>
2020-09-23 17:11:10 +0000

    s3a3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,20 +2,20 @@
 #include "holberton.h"
 
 /**
- *_islower - progam
- *return - 1 or 0
+ *_islower - progam return1 for lower case
+ *@c: Value of input
+ *Return: 1 or 0
  *
-*/
+ */
 
 int _islower(int c)
 {
-	
 	if (c >= 97 && c <= 122)
 	{
-		return 1;
-      	}
+		return (1);
+	}
 	else
 	{
-	return 0;
+		return (0);
 }
 }
