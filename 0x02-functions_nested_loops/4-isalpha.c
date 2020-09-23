62f0c9b3e65c0afc7642bdcf1b91eec94bae75cb
jdanielue <jdurregoe@gmail.com>
2020-09-23 18:32:36 +0000

    s4a3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,14 +10,14 @@
 
 int _isalpha(int c)
 {
-	if (c >=65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-	else if (c >=97 && c <= 122)
+	else if (c >= 97 && c <= 122)
 	{
-                return (1);
-        }
+		return (1);
+	}
 	{
 		return (0);
 }
