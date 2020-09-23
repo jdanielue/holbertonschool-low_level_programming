324623b86c884bec0180b2f47ca3ba879d868345
jdanielue <jdurregoe@gmail.com>
2020-09-23 00:00:13 +0000

    s1a1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_alphabet - program
+ *return: 0
+ *
+*/
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,1 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(int a);
+void print_alphabet(void);
+#endif
