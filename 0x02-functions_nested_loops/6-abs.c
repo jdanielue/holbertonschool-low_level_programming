bf72ab9293d8ca01f7ff4c803f6feadeb3875e54
jdanielue <jdurregoe@gmail.com>
2020-09-24 01:47:41 +0000

    s6a2

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include "holberton.h"
 
 /**
- *int _abs(int) - progam return1 for alpha case
+ *_abs - progam return1 for alpha case
  *@n: Value of input
  *Return: 1 or 0
  *
@@ -11,6 +11,7 @@
 int _abs(int n)
 {
 	int positive;
+
 	positive = n * (-1);
 
 	if (n < 0)
