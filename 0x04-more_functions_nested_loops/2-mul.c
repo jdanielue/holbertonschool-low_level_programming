64ddfd881657a4f9f5b27ee31ea1dc3cd076fa13
jdanielue <jdurregoe@gmail.com>
2020-09-25 04:40:11 +0000

    s2a1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *mul - check if is a digit.
+ *
+ * Return: Always c.
+ *@a : int character
+ *@b : int character
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
