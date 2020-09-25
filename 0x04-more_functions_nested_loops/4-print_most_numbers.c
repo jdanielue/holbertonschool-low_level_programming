e7767bc286f48855c97625dee6e5069d6ebd2481
jdanielue <jdurregoe@gmail.com>
2020-09-25 18:55:42 +0000

    s4a1

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *print_most_numbers - print numbers of 0 to 9
+ * @void: no parameter
+ *
+ * Return: not return
+ **/
+void print_most_numbers(void)
+{
+	int c;
+	for (c = 0; c < 2; c++)
+	{
+		_putchar(c + '0');
+	}
+	for (c = 3; c < 4; c++)
+	{
+                _putchar(c + '0');
+	}
+	for (c =5; c < 10; c++)
+	{
+                _putchar(c + '0');
+	}
+	_putchar('\n');
+}
