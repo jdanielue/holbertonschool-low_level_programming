073d1dd2fd9f546796fce6154f9a6eba57d28d3d
jdanielue <jdurregoe@gmail.com>
2020-09-25 19:07:23 +0000

    s4a2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,14 +9,10 @@
 void print_most_numbers(void)
 {
 	int c;
-	for (c = 0; c <= 3; c++)
+	for (c = 0; c < 10; c++)
 	{
-		if (c != 2)
+		if (c != 2 || c != 4)
 		_putchar(c + '0');
-	}
-	for (c =5; c < 10; c++)
-	{
-                _putchar(c + '0');
 	}
 	_putchar('\n');
 }
