358de50699c6536cb519f73e8b485cce4dd96aae
jdanielue <jdurregoe@gmail.com>
2020-09-25 04:52:02 +0000

    s3a1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_most_numbers - check if is a digit.
+ *
+ * Return: Always c.
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0 ; c <= 9 && c != 2 c != 4 ; c++)
+	{
+		_putchar(c)
+	}
+	return (0);
+}
