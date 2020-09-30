ec865fe556f4d577ecb55a83f84984f6d61a2e51
jdanielue <jdurregoe@gmail.com>
2020-09-30 23:31:10 +0000

    S8A3

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,7 +17,7 @@ void print_array(int *a, int n)
 		printf("%d", *(a + i));
 		printf(", ");
 	}
-	for (i = (n - 1); i <= (n - 1); i++)
+	for (i = 0; i == (n - 1); i++)
 	{
 		printf("%d", *(a + i));
 	}
