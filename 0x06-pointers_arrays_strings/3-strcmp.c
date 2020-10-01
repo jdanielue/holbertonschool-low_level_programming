a1674e502a21e0e1d516db86abb2acbb7b1f9f7a
jdanielue <jdurregoe@gmail.com>
2020-10-01 23:19:29 +0000

    s3a1

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *@a: variable
+ *@n: variable
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int m, i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if ( s1[i] == s2[i])
+		{
+			m = s1[i] - s2[i];
+		return (m);
+		}
+		else if (s1[i] < s2[i])
+		{
+		m = (s1[i] - s2[i]);
+		return (m);
+		}
+		else if (s1[i] > s2[i])
+		{
+		m = (s1[i] - s2[i]);
+		return (m);
+		}
+	}
+	return (0);
+}
