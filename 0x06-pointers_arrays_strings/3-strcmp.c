aa8c50dd2038e957cefa4bf0d9544b6dd3e3bbda
jdanielue <jdurregoe@gmail.com>
2020-10-01 23:25:40 +0000

    s3a2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *@a: variable
- *@n: variable
- * Return: Always 0.
+ *_strcmp - check the code for Holberton School students.
+ *@s1: variable
+ *@s2: variable
+ * Return: Always m.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if ( s1[i] == s2[i])
+		if (s1[i] == s2[i])
 		{
 			m = s1[i] - s2[i];
 		return (m);
@@ -29,5 +29,4 @@ int _strcmp(char *s1, char *s2)
 		return (m);
 		}
 	}
-	return (0);
 }
