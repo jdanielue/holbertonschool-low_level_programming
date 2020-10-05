c0c3bdafdedac894d7f586b44dbf437b8f06da40
jdanielue <jdurregoe@gmail.com>
2020-10-05 01:29:45 +0000

    s4a1

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,12 +3,22 @@
 
 /**
  *reverse_array - reverses the content of an array of integers
- *
- *
+ *@a: variable
+ *@n: variable
  *Return: No return
 */
 
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
-	
+	int aaux[100];
+	int i;
+	for (i = 1; i < n; i++)
+	{
+		aaux[i] = a[i];
+	}
+       	for (i = n; i > 0; i--)
+	{
+		a[i - 1] = aaux [n-i];
+	}
+
 }
