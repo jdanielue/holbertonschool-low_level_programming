68bb7655707057d5fe62d2f1329b0e9cc1393214
jdanielue <jdurregoe@gmail.com>
2020-10-07 02:39:30 +0000

    s3a4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int a = 0;
 	int i, n;
 
-	for (n = 0; *(s + n) != ' ' || *(s + n) != '\0'; n++)
+	for (n = 0; *(s + n) != ' '; n++)
 	{
 		for (i = 0; *(accept + i); i++)
 		{
