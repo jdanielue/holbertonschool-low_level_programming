47726f6c3c6f606dcaf66954882f58b5c5f6a2df
jdanielue <jdurregoe@gmail.com>
2020-10-09 03:07:55 +0000

    s6a3

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,14 +4,21 @@ int primo(int n, int x);
 /**
  * is_prime_number - returns the length of a string
  * primo - function that helps to find the prime number
- *@n: constant value to finde the natural square root
- *Return: _help
+ * @n: constant value to finde the natural square root
+ * Return: _help
  *
  */
 
 int is_prime_number(int n)
 {
-	return (primo(n, 1));
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (primo(n, 1));
+	}
 }
 int primo(int n, int x)
 {
