8abafa9cc5b20b495490ebb3519550899c07fdb6
jdanielue <jdurregoe@gmail.com>
2020-10-10 23:55:24 +0000

    s1a1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+B#include <stdio.h>
+#include "holberton.h"
+
+
+/**
+ * main - prints its name, followed by a new line.
+ *@argc:number of arguments
+ *@argv: pointer array
+ *Return: ALWAYS 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc - 1;)
+	{
+		if (argv[i] != NULL)
+			i++;
+	}
+	printf("%d\n", i);
+	return (0);
+}
