d5a1d5dadb2bf94b6841a4991c5d3f2870630f85
jdanielue <jdurregoe@gmail.com>
2020-10-14 00:04:51 +0000

    s1a1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * create_array - prints buffer in hexa
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_strdup(char *str)
+{
+	char *t;
+	int n, j;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+	}
+	if (n <= 0)
+	{
+		return (NULL);
+	}
+	n = n + 1;
+	t= malloc(n);
+	
+	for (j = 0; j <= n; j++)
+	{
+		t[j] = str[j];
+	}
+return (t);
+}
