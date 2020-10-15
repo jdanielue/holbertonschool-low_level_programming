d5aebb2dfbc83b8d15e650d0c30c55f3fad59fe2
jdanielue <jdurregoe@gmail.com>
2020-10-15 20:46:39 +0000

    s1a1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - check the code for Holberton School students.
+ *@s1: imput 1
+ *@s2: imput 2
+ *@n: size of imput comparation
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, m, t;
+	char *p;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (i = 0; s2[j] != '\0'; j++)
+	{
+	}
+	if (n > 0)
+		p = malloc(300);
+	for (m = 0; s1[m] != '\0'; m++)
+	{
+		p[m] = s1[m];
+	}
+	for (t = 0; s2[t] != '\0'; t++)
+	{
+	p[t + m] = s2[t];
+	}
+	return (p);
+}
