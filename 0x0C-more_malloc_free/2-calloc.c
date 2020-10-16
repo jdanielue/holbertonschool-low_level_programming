987361e916710920617bd43af0ce2ada8150915a
jdanielue <jdurregoe@gmail.com>
2020-10-16 20:44:24 +0000

    s2 a6

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
+	unsigned int n;
 
 	p = calloc(nmemb, size);
 	if (p == NULL)
 	{
 		return (NULL);
+	}
+	for ( n = 0; n <= size; n++)
+	{
+		p[n] = 0;
 	}
 	return (p);
 }
