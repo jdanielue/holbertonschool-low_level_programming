461fe0af54d04cc016f34a1b0f1df21387c98020
jdanielue <jdurregoe@gmail.com>
2020-10-16 04:19:22 +0000

    s1a4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,6 +3,24 @@
 #include <stdlib.h>
 
 /**
+ * length - check the code for Holberton School students.
+ *@s: imput 1
+ * Return: Always str.
+ */
+
+int length(char *s)
+{
+	int i, str;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		str++;
+	}
+	return (str);
+}
+
+
+/**
  * string_nconcat - check the code for Holberton School students.
  *@s1: imput 1
  *@s2: imput 2
@@ -14,24 +32,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, m, t;
 	char *p;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
+	i = length(s1);
+	j = length(s2);
+
 	if (n >= j)
-		p = malloc(j + n);
+	{
+		n = j;
+		p = malloc(300);
+	}
 	else
-		p = malloc(i + j);
-	printf("i %d, j %d, i + j %d, n%d \n", i, j, i + j, n);
-	for (m = 0; s1[m] != '\0'; m++)
+	{
+		p = malloc(300);
+	}
+	for (m = 0; m <= i; m++)
 	{
 		p[m] = s1[m];
 	}
-	for (t = 0; s2[t] != '\0'; t++)
+	for (t = 0; t <= (n + i); t++)
 	{
-	p[t+m] = s2[t];
+	p[t + m] = s2[t];
 	}
 	return (p);
 }
