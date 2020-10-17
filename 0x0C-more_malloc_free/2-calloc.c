8a839aa3e6a2b2764d76765d6f0dd6ac7cc4f801
jdanielue <jdurregoe@gmail.com>
2020-10-17 22:08:38 +0000

    s2a14

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,8 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int n;
 
 	if (nmemb == 0 || size == 0)
-		return(NULL);
-
+		return (NULL);
 	p = malloc(size * nmemb);
 	if (p == NULL)
 	{
