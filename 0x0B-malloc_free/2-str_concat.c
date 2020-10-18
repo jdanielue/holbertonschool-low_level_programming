63006fa44f643f4a560ae09f7f8fc39851a4c1c6
jdanielue <jdurregoe@gmail.com>
2020-10-18 03:46:32 +0000

    s2a22

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *t;
 
 	if (s1 == NULL)
-		return (NULL);
+	       s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
