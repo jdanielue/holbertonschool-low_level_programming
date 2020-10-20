2793a36c695a1f15eaa85ea937fee2dc4ac1521b
jdanielue <jdurregoe@gmail.com>
2020-10-20 22:14:15 +0000

    s0a1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+*/
+
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif
