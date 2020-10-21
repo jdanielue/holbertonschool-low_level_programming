6fbb0e198a0ee8298f03f7987462ba1d6809edfd
jdanielue <jdurregoe@gmail.com>
2020-10-21 22:27:45 +0000

    s1a3

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,9 +16,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL)
 	{
+		return;
 	}
 	if (action == NULL)
 	{
+		return;
 	}
 		for (i = 0; i < size; i++)
 		{
