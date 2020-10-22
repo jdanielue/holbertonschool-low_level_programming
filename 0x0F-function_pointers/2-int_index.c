4730cd6cc6b0b1df3c9f7ad9c42f3e3cbab62f95
jdanielue <jdurregoe@gmail.com>
2020-10-22 01:52:45 +0000

    s2a1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - prints a name as is
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer to a function
+ *
+ * Return: Nothing.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i, v;
+
+if (cmp == NULL)
+{
+}
+if (array == NULL)
+{
+}
+if (size <= 0)
+return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		v = cmp(array[i]);
+		if (v != 0)
+		{
+			return (i);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+}
