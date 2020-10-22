6cc873f3ea30dcbc3d77a06b9c6846c96c4243b7
jdanielue <jdurregoe@gmail.com>
2020-10-22 16:36:19 +0000

    s2a9

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,9 +15,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i, v;
 
 	if (cmp == NULL)
-		return (NULL);
+		return (-1);
 	if (array == NULL)
-		return (NULL);
+		return (-1);
 	if (size <= 0)
 	return (-1);
 	for (i = 0; i < size; i++)
