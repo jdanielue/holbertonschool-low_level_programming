a99837299a7432f999feb0559a73ec348863e887
jdanielue <jdurregoe@gmail.com>
2020-10-26 04:21:54 +0000

    s3a1

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - c
+ * @format: nose
+ * Return: Always 0.
+ */
+
+
+void print_all(const char * const format, ...)
+{
+
+int n;
+
+va_list datos;
+va_start (datos, format);
+
+n = 0;
+while (format[n] != '\0')
+{
+	if (n == 0)
+	{
+		switch (*(format + n))
+	{
+	case 'c':
+		printf("%c", va_arg(datos, int));
+		break;
+	case 'i':
+		printf("%d", va_arg(datos, int));
+		break;
+	case 'f':
+		printf("%f", va_arg(datos, double));
+		break;
+	case 's':
+		printf("%s", va_arg(datos, char*));
+		break;
+	default:
+		break;
+	}
+	}
+	else
+	{
+		switch (*(format + n))
+	{
+	case 'c':
+		printf(", %c", va_arg(datos, int));
+		break;
+	case 'i':
+		printf(", %d", va_arg(datos, int));
+		break;
+	case 'f':
+		printf(", %f", va_arg(datos, double));
+		break;
+	case 's':
+		printf(", %s", va_arg(datos, char*));
+		break;
+	default:
+		break;
+	}
+	}
+	n++;
+}
+printf("\n");
+va_end (datos);
+
+
+}
