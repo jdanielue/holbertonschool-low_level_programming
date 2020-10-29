ba1fa186008e6c56fe628105e9d6e043a0802493
jdanielue <jdurregoe@gmail.com>
2020-10-29 06:17:25 +0000

    file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,26 +10,27 @@
 
 int main(int argc, char *argv[])
 {
-int c, d;
+int num1, num3;
+num1 = atoi(argv[1]);
+
+num3 = atoi(argv[3]);
 
 	if (argc != 4)
 		{
 		printf("Error\n");
-		return (98);
+		exit(98);
 		}
 	if ((argv[2]) == NULL)
 		{
 		printf("Error\n");
-		return (99);
+		exit(99);
 		}
 
-	if ((argv[3]) == '0' && ((argv[2][0]) == '%' || (argv[2][0]) == '/'))
+	if (num3 == 0 && ((argv[2][0]) == '%' || (argv[2][0]) == '/'))
 	{
 		printf("Error\n");
-		return (99);
+		exit(100);
 	}
-	c = atoi(argv[1]);
-	d = atoi(argv[3]);
-	printf("%d\n", get_op_func(argv[2])(c, d));
+	printf("%d\n", get_op_func(argv[2])(num1, num3));
 	return (0);
 }
