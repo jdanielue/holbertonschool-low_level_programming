c62c839ac43f93960bf564ddb994eef8d3f47141
jdanielue <jdurregoe@gmail.com>
2020-10-31 02:14:01 +0000

    allfiles and o excersice

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - singly linked list
+ * @h: string - (malloc'ed string)
+ *Return: number of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nill)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (i);
+}
