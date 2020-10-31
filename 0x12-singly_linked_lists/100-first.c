bb04b61b1439d5beb7f7661819b9ee04c5298a3e
jdanielue <jdurregoe@gmail.com>
2020-10-31 04:13:02 +0000

    100done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - check the code for Holberton School students.
+ *
+ * Return: none
+ */
+
+void myStartupFun(void)
+{
+printf("You're beat! and yet, you must allow\n");
+printf("I bore my house upon my back!\n");
+}
