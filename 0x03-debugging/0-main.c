25166be2ddf5b700fb429df0f9a9590946fd404f
jdanielue <jdurregoe@gmail.com>
2020-11-02 03:57:58 +0000

    s0a2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,16 @@
 #include "holberton.h"
 
 /**
-* positive_or_negative - integer is positive or negative
-*@i: number
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
-void positive_or_negative(int i)
+int main(void)
 {
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	} else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	} else
-	{
-		printf("%d is negative\n", i);
-	}
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
 }
