8a254d46c4706020a78eaa574f5b79164d0e9fe6
jdanielue <jdurregoe@gmail.com>
2020-11-02 07:17:31 +0000

    s9a1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -36,9 +36,9 @@ void times_table(void)
 		}
 		if (resultado < 10 && n < 9)
 		{
+			_putchar(32);
 			_putchar(resultado + '0');
 			_putchar(',');
-			_putchar(32);
 			_putchar(32);
 		}
 		if (resultado < 10 && n == 9)
