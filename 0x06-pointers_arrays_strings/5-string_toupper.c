fa26941221dba7b0f1c9f81b635190e1ab3b0378
jdanielue <jdurregoe@gmail.com>
2020-11-09 00:11:10 +0000

    s5a2

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - check the code for Holberton School students.
+ * @str: string 2
+ * Return: str.
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		*(str + i) = *(str + i) - 32;
+	}
+	return (str);
+}
