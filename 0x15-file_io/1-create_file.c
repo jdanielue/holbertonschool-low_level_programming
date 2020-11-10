6661ce045fac907d97f14ded1a16b4677eaeeaf0
jdanielue <jdurregoe@gmail.com>
2020-11-10 05:03:07 +0000

    s1a1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - check the code for Holberton School students.
+ * @filename: file name
+ * @text_content: text
+ * Return: int.
+ */
+
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, i;
+
+	for (i = 0; *(text_content + i) != '\0'; i++)
+	;
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+
+	if (text_content != '\0')
+	{
+		write(fd, text_content, i);
+	}
+
+	if (fd == 0)
+		return (-1);
+	if (fd == -1)
+		return (0);
+
+		close(fd);
+
+	return (fd);
+}
