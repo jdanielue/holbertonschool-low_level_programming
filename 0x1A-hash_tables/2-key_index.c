f4faa28ccc618439a3e19ee99122ff08ebcfa669
jdanielue <jdurregoe@gmail.com>
2021-01-15 15:11:54 -0500

    s2a3 correct output

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,9 +11,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
+	index = hash_djb2(key) % size;
 
-	if (key == NULL || size == 0)
-		return NULL;
-	index = *key % size;
 	return (index);
 }
