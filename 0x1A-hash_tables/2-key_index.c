436f1ecb98e7baf9e0ab951f7c11dfc4be0da064
jdanielue <jdurregoe@gmail.com>
2021-01-15 14:42:28 -0500

    s2a1 edge case

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,6 +12,8 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
 
+	if (key == NULL || size == 0)
+		return NULL;
 	index = *key % size;
 	return (index);
 }
