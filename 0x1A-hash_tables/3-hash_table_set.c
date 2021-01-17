8c8f7147481c27d761fa8f9a6b8e309423025805
jdanielue <jdurregoe@gmail.com>
2021-01-16 21:33:11 -0500

    s3a1 head

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *pt_newnode, *head;
+	hash_node_t *pt_newnode, head;
 	unsigned long int index = 0;
 
 	if (ht == NULL)
