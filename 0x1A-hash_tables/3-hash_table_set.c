fc1e57cb76305528078ef6a4885f76f38ae44977
jdanielue <jdurregoe@gmail.com>
2021-01-16 22:58:42 -0500

    s3a7 ==

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,6 +10,8 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+
+	
 	hash_node_t *pt_newnode;
 	unsigned long int index = 0;
 
@@ -17,20 +19,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	pt_newnode = malloc(sizeof(hash_node_t));
-	if (pt_newnode != NULL)
+	if (pt_newnode == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	
-	strcpy(pt_newnode->key, key);
+	pt_newnode->key = strdup(key);
 	if (pt_newnode->key == NULL)
 		return (0);
-	strcpy(pt_newnode->value, value);
+	pt_newnode->value = strdup(value);
 	if (pt_newnode->value == NULL)
 		return (0);
 	pt_newnode->next = NULL;
-
 
 	pt_newnode->next = *(ht->array + index);
 	*(ht->array + index) = pt_newnode;
