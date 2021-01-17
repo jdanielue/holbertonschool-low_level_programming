4e60cbd19e961f0c2aabf1aeb870b46f2ba12162
jdanielue <jdurregoe@gmail.com>
2021-01-17 18:57:39 -0500

    s3a12 all in one

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,59 +10,37 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *pt_newnode;
+	hash_node_t *pt_newnode, *head;
+	unsigned long int index;
 
+	index = key_index((const unsigned char *)key, ht->size);
 	if (ht == NULL)
 		return (0);
 
-	pt_newnode = new_node(ht, key, value);
-	if (pt_newnode == NULL)
-		return (0);
-
-	if (pt_newnode->key == NULL)
-		return (0);
-	if (pt_newnode->value == NULL)
-		return (0);
-	pt_newnode->next = NULL;
-	return (1);
-}
-
-/**
- * new_node -  function that adds an element to the hash table.
- * @key: key
- * @value: value of the key
- * @ht: table
- * Return: element of the table.
-*/
-
-hash_node_t *new_node(hash_table_t *ht, const char *key, const char *value)
-{
-	hash_node_t *pt_newnode, *head, *temp;
-	unsigned long int index = 0;
-
-
-	index = key_index((const unsigned char *)key, ht->size);
 	head = *(ht->array + index);
-	temp = head;
-
 	while (head)
 	{
 		if (!strcmp(key, head->key))
 		{
 			free(head->value);
 			head->value = strdup(value);
-			return (head);
+			return (1);
 		}
 		head = head->next;
 	}
 
 	pt_newnode = malloc(sizeof(hash_node_t));
-	
-	pt_newnode->key = strdup(key);
-	pt_newnode->value = strdup(value);
-	pt_newnode->next = NULL;
-	pt_newnode->next = temp;
-	temp = pt_newnode;
 
-	return (pt_newnode);
+	if (pt_newnode == NULL)
+		return (0);
+	pt_newnode->key = strdup(key);
+	if (pt_newnode->key == NULL)
+		return (0);
+	pt_newnode->value = strdup(value);
+	if (pt_newnode->value == NULL)
+		return (0);
+	pt_newnode->next = NULL;
+	pt_newnode->next = *(ht->array + index);
+	*(ht->array + index) = pt_newnode;
+	return (1);
 }
