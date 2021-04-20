255d4a66f4191536f5c951bd0827e81cb0b4f95c
jdanielue <jdurregoe@gmail.com>
2021-04-20 14:39:42 -0500

    s1 a3

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 	while (izq < der)
 	{
 		med = (der + izq) / 2;
-		printf("Searching in array:");
+		printf("Searching in array: ");
 		for (i = izq; i < der; i++)
 		{
 			printf("%d, ", array[i]);
