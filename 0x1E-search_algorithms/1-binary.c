83790af9d497353ffe2e0e2165c6b30ca1c7f1e7
jdanielue <jdurregoe@gmail.com>
2021-04-20 14:35:46 -0500

    s0 s1 a0

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,35 +8,38 @@
  * Return: -1 when fail
  */
 
-int binary_search(int *array, size_t size, int value){
+int binary_search(int *array, size_t size, int value)
+{
+	int izq, der, med;
+	int i;
 
-    int izq, der, med;
-    int i;
+	izq = 0;
+	der = size - 1;
 
-    izq = 0;
-    der = size -1;
+	if (array == NULL)
+		return (-1);
 
-    while (izq < der)
-    {
-        med = (der + izq) / 2;
-        printf("Searching in array:");
-        for (i = izq; i < der; i++)
-        {
-            printf("%d, ", array[i]);
-        }
-        printf("%d", array[der]);
-        printf("\n");
+	while (izq < der)
+	{
+		med = (der + izq) / 2;
+		printf("Searching in array:");
+		for (i = izq; i < der; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d", array[der]);
+		printf("\n");
 
-        if (array[med] == value)
-            return med;
-        else if (array[med] > value)
-        {
-            der = med - 1;
-        }
-        else
-        {
-            izq = med + 1;
-        }
-    }
-    return -1;
+		if (array[med] == value)
+			return (med);
+		else if (array[med] > value)
+		{
+			der = med - 1;
+		}
+		else
+		{
+			izq = med + 1;
+		}
+	}
+	return (-1);
 }
